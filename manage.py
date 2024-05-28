5f04f345203eb03c71dfe5f5d3ac27a10826a620
Jose Angel Hernández <ja.hernandezds@gmail.com>
2024-05-27 23:13:25 -0400

    settings as local

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
 import subprocess
 
-# Ejemplo para ejecutar un script de Python desde otro script
 subprocess.run(["python", "tabbycat/manage.py", "runserver"])
--- tabbycat/settings/github.py
+++ tabbycat/settings/github.py
@@ -1,7 +1,11 @@
 # ==============================================================================
 # Github CI
 # ==============================================================================
+# ==============================================================================
+# Settings that you should specify
+# ==============================================================================
 
+# SQL SERVER ENGINE
 
 DATABASES = {
     'default': {
@@ -18,3 +22,36 @@ DATABASES = {
         },
     }
 }
+
+# Replace this with your time zone, as defined in the IANA time zone database:
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List
+TIME_ZONE = 'Australia/Melbourne'
+
+
+# ==============================================================================
+# Django-specific Modules
+# ==============================================================================
+
+ALLOWED_HOSTS = ["*"]
+
+INTERNAL_IPS = (
+    '0.0.0.0',
+    '127.0.0.1'
+)
+
+# ==============================================================================
+# Caching
+# ==============================================================================
+
+PUBLIC_FAST_CACHE_TIMEOUT   = 0
+PUBLIC_SLOW_CACHE_TIMEOUT   = 0
+TAB_PAGES_CACHE_TIMEOUT     = 0
+
+CACHES = { # Use a dummy cache in development
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+   }
+}
+
+# Use the cache with database write through for local sessions
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
